5b5209d31703720354d5776c94d87449efa7dbd0
Daniel Felipe Payares Ramos <dfelipe.payares@udea.edu.co>
2026-09-09 20:23:07 -0500

    Mi primer commit en Git

---
+++ ejercicio2_1/build/Desktop_Qt_6_11_2_MinGW_64_bit_Debug/CMakeFiles/ejercicio2_1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ejercicio2_1.dir/main.cpp.obj"
+  "CMakeFiles/ejercicio2_1.dir/main.cpp.obj.d"
+  "ejercicio2_1.exe"
+  "ejercicio2_1.exe.manifest"
+  "ejercicio2_1.pdb"
+  "libejercicio2_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ejercicio2_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ problema_1.cpp
@@ -0,0 +1,28 @@
+/*
+Desarrolle un programa que permita determinar la m´ınima combinaci´on de billetes y monedas para
+una cantidad de dinero determinada. Los billetes en circulaci´on son de $50.000, $20.000, $10.000,
+$5.000, $2.000 y $1.000, y las monedas son de $500, $200, $100 y $50. Si por medio de los billetes
+y monedas disponibles no se puede lograr la cantidad deseada, el sistema deber´a decir lo que resta
+para lograrla. Use arreglos y ciclos para realizar el programa.
+Por ejemplo, si se ingresa 47810, el programa debe imprimir:
+50000:0
+20000:2
+10000:0
+5000:1
+2000:1
+1000:0
+500:1
+200:1
+100:1
+50:0
+Faltante: 10
+*/
+
+#include <iostream>
+using namespace std;
+int main(){
+    int dineoro[10] = {50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50};
+    
+
+    return 0;
+}
